909d835313e8ea74cc4afb676704fbefb7ac3c3e
ahmdrsln <ahmadruslanamad4@gmail.com>
2019-09-23 15:36:40 +0700

    :git_pertama

---
+++ resources/views/halamanbaru.blade.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HAI! &mdash; Selamat datang di blog saya!</title>
+    <meta name="description" content="Free Bootstrap Theme by uicookies.com">
+    <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+    
+    <link href="https://fonts.googleapis.com/css?family=Inconsolata|Rubik:300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles-merged.css">
+    <link rel="stylesheet" href="css/style.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!--[if lt IE 9]>
+      <script src="js/vendor/html5shiv.min.js"></script>
+      <script src="js/vendor/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+  <!-- START: header -->
+
+  <header role="banner" class="probootstrap-header">
+    <div class="container-fluid">
+        <a href="index.html" class="probootstrap-logo">Blog<span>.</span></a>
+        
+        <a href="#" class="probootstrap-burger-menu visible-xs" ><i>Menu</i></a>
+        <div class="mobile-menu-overlay"></div>
+
+        <nav role="navigation" class="probootstrap-nav hidden-xs">
+          <ul class="probootstrap-main-nav">
+            <li class="active"><a href="index.html">Home</a></li>
+            <li><a href="programs.html">Pengalaman PKL</a></li>
+            <li><a href="gallery.html">Gallery</a></li>
+            <li><a href="about.html">Tentang saya</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+          <ul class="probootstrap-header-social hidden-xs">
+            <li><a href="#"><i class="icon-twitter"></i></a></li>
+            <li><a href="#"><i class="icon-facebook2"></i></a></li>
+            <li><a href="#"><i class="icon-instagram2"></i></a></li>
+          </ul>
+          <div class="extra-text visible-xs">
+            <a href="#" class="probootstrap-burger-menu"><i>Menu</i></a>
+            <h5>Connect With Me</h5>
+            <ul class="social-buttons">
+              <li><a href="#"><i class="icon-twitter"></i></a></li>
+              <li><a href="#"><i class="icon-facebook2"></i></a></li>
+              <li><a href="#"><i class="icon-instagram2"></i></a></li>
+            </ul>
+          </div>
+        </nav>
+    </div>
+  </header>
+  <!-- END: header -->
+  
+  <!-- START: section -->
+  <section class="probootstrap-intro" style="background-image: url(img/hero_bg_1_b.jpg);" data-stellar-background-ratio="0.5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-7 probootstrap-intro-text">
+        <h1 class="probootstrap-animate" data-animate-effect="fadeIn"><span>Ahmad Ruslan</span>Ismayana</h1>
+          <div class="probootstrap-subtitle probootstrap-animate" data-animate-effect="fadeIn">
+            <h2>FreshGraduate</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <span class="probootstrap-animate"><a class="probootstrap-scroll-down js-next" href="#next-section">Scroll down <i class="icon-chevron-down"></i></a></span>
+  </section>
+  <!-- END: section -->
+  <main>
+    <section id="next-section" class="probootstrap-section">
+      <div class="container">
+        <div class="row probootstrap-gutter60 mb50">
+          <div class="col-md-6">
+            <figure><img src="img/jas1.jpg" lt="Free Bootstrap Template by uicookies.com" class="img-responsive"></figure>
+          </div>
+          <div class="col-md-6">
+            <h2 class="probootstrap-heading">Tentang Saya</h2>
+            <p>Nama   : Ahmad Ruslan Ismayana</p><br>
+            <p>Tempat,Tgl Lahir : Balikpapan, 30 Maret 2001</p><br><p>Jenis Kelamin  : Laki-Laki</p><br><p>Tinggi & Berat badan : 169cm & 65 kg </p><br><p>Agama   : Islam</p><br><p> Kewarganegaraan : Indonesia</p><br><p> Status   : Belum Menikah</p><br><p> Alamat : Perumnas, Jl. Meranti Raya Rt. 46 No. 33   Batu Ampar, Kota Balikpapan 76126</p> 
+            <p><a href="#" class="btn btn-primary">Read more about me</a></p>
+          </div>
+        </div>
+
+        <div class="row probootstrap-gutter60">
+          <div class="col-md-4 mb30">
+            <h4 class="mb30">My Mission</h4>
+            <p>&ldquo;.&rdquo;</p>
+          </div>
+          <div class="col-md-4 mb30">
+            <h4 class="mb30">Pendidikan</h4>
+            <ul class="probootstrap-blog-list">
+              <li>
+                <a href="#">
+                  <figure class="probootstrap-image"><img src="img/sd.png" alt="Free Bootstrap Template by uicookies.com" class="img-responsive"></figure>
+                  <div class="probootstrap-text">
+                    <h4>SDN 011 Balikpapan Timur</h4>
+                    <span class="meta">2007-2013</span>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <figure class="probootstrap-image"><img src="img/smp8.jpeg" alt="Free Bootstrap Template by uicookies.com" class="img-responsive"></figure>
+                  <div class="probootstrap-text">
+                    <h4>SMPN 8 Balikpapan</h4>
+                    <span class="meta">2013-2016</span>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <figure class="probootstrap-image"><img src="img/smk1.png" alt="Free Bootstrap Template by uicookies.com" class="img-responsive"></figure>
+                  <div class="probootstrap-text">
+                    <h4>SMKN 1 Balikpapan</h4>
+                    <span class="meta">2016-2019</span>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-4 mb30">
+            <h4 class="mb30">My Vision</h4>
+            <p>&ldquo;.&rdquo;</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="probootstrap-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 probootstrap-relative">
+            <h2 class="probootstrap-heading mt0 mb50">Pengalaman PKL</h2>
+            <ul class="probootstrap-owl-navigation absolute right">
+              <li><a href="#" class="probootstrap-owl-prev"><i class="icon-chevron-left"></i></a></li>
+              <li><a href="#" class="probootstrap-owl-next"><i class="icon-chevron-right"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 probootstrap-relative">
+            <div class="owl-carousel owl-carousel-carousel">
+              <div class="item">
+                <div class="probootstrap-program">
+                  <a href="#"><img src="img/telkom.jpg" alt="Free Bootstrap Template by uicookies.com" class="img-responsive img-rounded"></a>
+                  <h3>PT TELKOM DIVRE VI BALIKPAPAN</h3>
+                  <p>April - Juni 2018</p>
+                  <p>Bidang Telekomunikasi                                                  Melakukan Cabling, Melakukan instalasi                                                   PC, Instalasi operating system,Trouble shooting</p>
+                </div>
+              </div>
+              <div class="item">
+                <div class="probootstrap-program">
+                  <a href="#"><img src="img/icon.jpeg" alt="Free Bootstrap Template by uicookies.com" class="img-responsive img-rounded"></a>
+                  <h3>PT INDONESIA COMNETS PLUS</h3>
+                  <p>Oktober - Desember 2018</p>
+                  <p>Bidang Operasi, pemeliharaan & asetMelakukan pendataan perangkat jaringan,Aktivasi data perusahaan, Membuat simulasi jalur aktivasi pelanggan di aplikasi</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  <footer class="probootstrap-footer">
+      <div class="probootstrap-backtotop"><a href="#" class="js-backtotop"><i class="icon-chevron-thin-up"></i></a></div>
+      <div class="container">
+      <div class="row mb50">
+        <div class="col-md-3">
+          <div class="probootstrap-footer-widget">
+            <h4>About The Site</h4>
+            <p>Berisi tentang biodata penulis. <a href="#">Learn More</a></p>
+          </div>
+        </div>
+        <div class="col-md-3 col-md-push-1">
+          <div class="probootstrap-footer-widget">
+            <h4>Active Hours</h4>
+            <ul class="probootstrap-footer-link">
+              <li>Week Days: 05:00 – 22:00</li>
+              <li>Saturday: Rest day</li>
+              <li>Sunday: Holiday</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-5 col-md-push-1">
+          <div class="probootstrap-footer-widget">
+            <h4>Links</h4>
+            <ul class="probootstrap-footer-link float">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Pengalaman PKL</a></li>
+              <li><a href="#">Gallery</a></li>
+            </ul>
+            <ul class="probootstrap-footer-link float">
+              <li><a href="#">About Me</a></li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="#">Privacy</a></li>
+            </ul>
+          </div>
+          <div class="probootstrap-footer-widget">
+            <h4>Info Kontak</h4>
+            <ul class="probootstrap-contact-info">
+              <li><i class="icon-location2">Jalan Meranti Raya RT 46 NO 33,Perumnas Balikpapan Utara</i> <span></span></li>
+              <li><i class="icon-mail"></i><span>ahmadruslanamad4@gmail.com</span></li>
+              <li><i class="icon-phone2"></i><span>+6289 9980 51561</span></li>
+            </ul>
+          </div>
+        </div>
+        
+      </div>
+       <div class="row">
+         <div class="col-md-12 text-center border-top">
+           <p class="mb0">&copy; Copyright 2019 <a href="https://uicookies.com/"></a>. All Rights Reserved.
+         </div>
+       </div>
+     </div>
+  </footer>  
+
+  <script src="js/scripts.min.js"></script>
+  <script src="js/main.min.js"></script>
+  <script src="js/custom.js"></script>
+
+  </body>
+</html>
